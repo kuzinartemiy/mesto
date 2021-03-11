b69257e211805ba79128795a368e1864d55ccc44
kuzinartemiy <kuzin_artemiy@list.ru>
2021-03-11 13:51:07 +0500

    feat: добавить блок places и блок footer

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ let content = document.querySelector('.content');
 let editButton = content.querySelector('.profile__edit-button');
 let popup = content.querySelector('.popup');
 let closeButton = content.querySelector('.popup__close-button');
-let name = content.querySelector('.profile__name');
-let job = content.querySelector('.profile__job');
-let inputName = content.querySelector('.popup__input-name');
-let inputJob = content.querySelector('.popup__input-job');
-
-inputName.setAttribute('placeholder', name.textContent);
-inputJob.setAttribute('placeholder', job.textContent);
+let nameProfile = content.querySelector('.profile__name');
+let jobProfile = content.querySelector('.profile__job');
+let nameInput = content.querySelector('.popup__input-name');
+let jobInput = content.querySelector('.popup__input-job');
+let formElement = content.querySelector('.popup__form');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -18,5 +16,21 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+
+  let nameInputValue = nameInput.value;
+  let jobInputValue = jobInput.value;
+  
+  nameProfile.textContent = nameInputValue;
+  jobProfile.textContent = jobInputValue;
+
+  closePopup();
+}
+
 editButton.addEventListener('click',openPopup);
 closeButton.addEventListener('click',closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
+nameInput.setAttribute('value', nameProfile.textContent);
+jobInput.setAttribute('value', jobProfile.textContent);
